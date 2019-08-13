1a7cfb6154da649e86b712ae17fb484d753a67ea
xgan68 <ganx-68@live.cn>
2019-08-13 17:06:28 +0800

    加入了手机验证码登录页面

---
+++ Assets/Source/NetworkController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkController : MonoBehaviour
+{
+    public static NetworkController Instance;
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+
+    public void PostPhoneNumber(string phoneNumber, System.Action callback)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("phoneNumber", phoneNumber);
+
+        Debug.Log(phoneNumber);
+
+        //StartCoroutine(Upload(form), null);
+    }
+
+    public void PostVerificationCode(string code, System.Action callback)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("code", code);
+
+        Debug.Log(code);
+
+        //StartCoroutine(UploadVerificationCode(form), callback);
+    }
+
+    IEnumerator UploadPhoneNumber(WWWForm form, System.Action callback)
+    {
+        string url = "";
+        UnityWebRequest www = UnityWebRequest.Post(url, form);
+
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+    }
+
+    IEnumerator UploadVerificationCode(WWWForm form, System.Action<bool> callback)
+    {
+        string url = "";
+        UnityWebRequest www = UnityWebRequest.Post(url, form);
+
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            callback(false);
+            Debug.Log(www.error);
+        }
+        else
+        {
+            callback(true);
+            Debug.Log("Form upload complete!");
+        }
+    }
+
+}
---
+++ Assets/Source/PhoneLogin.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneLogin : MonoBehaviour
+{
+    public const float SEND_CODE_COOLDOWN_PERIOD = 5f;
+    public const string SEND_CODE_TEXT = "发送验证码";
+
+    [SerializeField]
+    private Button m_sendCodeButton;
+    [SerializeField]
+    private Button m_loginButton;
+    [SerializeField]
+    private Text m_sendCodeText;
+    [SerializeField]
+    private InputField m_phoneNumberInput;
+    [SerializeField]
+    private InputField m_codeInput;
+    [SerializeField]
+    private GameObject m_wrongVerificationCodeText;
+
+    private void Start()
+    {
+       // m_sendCodeButton.interactable = false;
+    }
+
+    public void OnSendCodeButtonClicked()
+    {
+        if (IsPhoneNumberValid(m_phoneNumberInput.text))
+        {
+            NetworkController.Instance.PostPhoneNumber(m_phoneNumberInput.text, delegate { });
+            StartCoroutine(SendCodeButtonCoolDown());
+        }
+    }
+
+    public void OnLoginButtonClicked()
+    {
+        NetworkController.Instance.PostVerificationCode(m_codeInput.text, ShowWorngCodeText);
+    }
+
+    public void ShowWorngCodeText()
+    {
+        m_wrongVerificationCodeText.SetActive(true);
+    }
+
+    public void OnCloseButtonClicked()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    IEnumerator SendCodeButtonCoolDown()
+    {
+        float cooldown = 0;
+        m_sendCodeButton.interactable = false;
+        m_sendCodeText.text = SEND_CODE_TEXT + "(" + SEND_CODE_COOLDOWN_PERIOD.ToString() + ")";
+
+        while (cooldown < SEND_CODE_COOLDOWN_PERIOD)
+        {
+            yield return new WaitForSeconds(1);
+            cooldown++;
+            m_sendCodeText.text = SEND_CODE_TEXT + "(" + (SEND_CODE_COOLDOWN_PERIOD - cooldown).ToString() + ")";
+        }
+       
+        m_sendCodeText.text = SEND_CODE_TEXT;
+        m_sendCodeButton.interactable = true;
+    }
+
+    private bool IsPhoneNumberValid(string phoneNumber)
+    {
+        bool isValid = false;
+
+        if (phoneNumber.Length == 11)
+        {
+            isValid = true;
+        }
+
+        return isValid;
+    }
+}
