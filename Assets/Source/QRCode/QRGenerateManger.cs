8b8d2dd2465416a620b71ddce568753cae3a7423
xgan68 <ganx-68@live.cn>
2019-08-20 15:44:21 +0800

    -加入了生成二维码的功能

---
+++ Assets/Source/QRCode/QRGenerateManger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing.Common;
+using ZXing;
+using UnityEngine.UI;
+
+
+public class QRGenerateManger : MonoBehaviour
+{
+    public static void GenerateQRImage(RawImage QRCodeImage, string content, int width, int height)
+    {
+        EncodingOptions options = null;
+        BarcodeWriter writer = new BarcodeWriter();
+        options = new EncodingOptions
+        {
+            Width = width,
+            Height = height,
+            Margin = 1,
+        };
+        options.Hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+        writer.Format = BarcodeFormat.QR_CODE;
+        writer.Options = options;
+        Color32[] colors = writer.Write(content);
+        
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels32(colors);
+        texture.Apply();
+        QRCodeImage.texture = texture;
+    }
+}
---
+++ Assets/Source/QRCode/QRScannerUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QRScannerUI : AUIPage
+{
+    [SerializeField]
+    private Text m_QRCodeID;
+
+    private void Start()
+    {
+        QRScanManager.QRCodeScanSuccess += UpdateQRCodeID;
+    }
+
+    private void UpdateQRCodeID(string ID)
+    {
+        m_QRCodeID.text = ID;
+    }
+
+    public void OnCloseButtonClicked()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
